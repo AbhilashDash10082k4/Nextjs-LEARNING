c373984cba7afa81de31abcf19a71d58bade1944
AbhilashDash10082k4 <abhilash10082k4@gmail.com>
2025-06-08 00:25:37 +0530

    added a customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,10 +1,13 @@
+import { fetchFilteredCustomers } from '@/app/lib/data';
+import { customers } from '@/app/lib/placeholder-data'
 import CustomersTable from '@/app/ui/customers/table'
 import React from 'react'
 
-function CustomerPage() {
+async function CustomerPage() {
+  const customers = await fetchFilteredCustomers("");
   return (
     <div>
-      {/* <CustomersTable customers={customers}/> */}
+      <CustomersTable customers={customers}/>
     </div>
   )
 }
